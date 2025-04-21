53f704ce01b94c3afef5ae6e1d0ec5fd49e8a52d
seekwhencer <matthias.kallenbach@gmail.com>
2025-04-21 19:22:27 +0200

    fix vite html base gh pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
     //root: '/app/frontend',
+    base: '/skwncr/',
     site: 'http://frontend.apply-o-mat.servant',
     output: "browser",
     security: {
