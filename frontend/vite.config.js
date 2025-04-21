fee3adb1243a965cba2cb307f9750d68c92a2c8c
seekwhencer <matthias.kallenbach@gmail.com>
2025-04-21 16:59:03 +0200

    vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,14 @@
 import {defineConfig} from 'vite';
-import { patchCssModules } from 'vite-css-modules'
 
 // https://vitejs.dev/config/
 
 export default defineConfig({
-    root: '/app/frontend',
+    //root: '/app/frontend',
     site: 'http://frontend.apply-o-mat.servant',
     output: "browser",
     security: {
         checkOrigin: false
     },
-    plugins: [
-        patchCssModules()
-    ],
     server: {
         host: true,
         port: 8080,
