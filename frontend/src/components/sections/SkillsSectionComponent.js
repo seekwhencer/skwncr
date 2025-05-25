807f266b1c3bbc135aa18ae7d7c802d39909d5bb
seekwhencer <matthias.kallenbach@gmail.com>
2025-05-25 20:41:17 +0200

    searchable skills

--- frontend/src/components/sections/SkillsSectionComponent.js
+++ frontend/src/components/sections/SkillsSectionComponent.js
@@ -7,7 +7,46 @@ export default class SkillsSectionComponent extends SectionComponent {
         this.target = this.options.target ? this.options.target : this.parent;
         this.element = document.querySelector('[data-section=skills]');
 
+        // behavior
+        this.element.onclick = (e) => this.select(e);
         //
-        this.element.querySelectorAll('.skill').forEach(s => s.onclick = () => s.classList.toggle('like'));
+        this.searchInput = this.element.querySelector('[data-skills-search]');
+        this.searchInput.onkeyup = this.searchInput.onblur = () => this.search();
+
+        this.skills = this.element.querySelectorAll('.skill');
+    }
+
+    select(e) {
+        e.preventDefault();
+        if (e.target === e.currentTarget) {
+            this.unselectAll();
+        }
+
+        if (e.target.classList.contains('skill')) {
+            e.target.classList.toggle('like');
+        }
+
+    }
+
+    unselectAll() {
+        this.skills.forEach(s => s.classList.remove('like'));
+    }
+
+    search() {
+        const val = this.searchInput.value.toLowerCase();
+        this.skills.forEach(s => {
+            if (s.dataset.text.toLowerCase().includes(val)) {
+                s.classList.remove('hidden');
+
+                if (val !== '') {
+                    s.classList.add('match');
+                } else {
+                    s.classList.remove('match');
+                }
+            } else {
+                s.classList.add('hidden');
+                s.classList.remove('match');
+            }
+        });
     }
 }
